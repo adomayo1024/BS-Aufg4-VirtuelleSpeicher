9c4ac589e84fb85d3a5d2412b434f4e9ea21fea9
adomayo1024 <git.hub.leon@gmail.com>
2024-12-02 11:40:07 +0100

    Messungen durchgeführt

--- src/main/java/de/hawhh/osbsp/SimulationEnv.java
+++ src/main/java/de/hawhh/osbsp/SimulationEnv.java
@@ -47,15 +47,15 @@ public class SimulationEnv {
         simulationTime = 200;
         // max. Anzahl Seiten pro Prozess im Hauptspeicher (sonst Verdr�ngung
         // eigener Seiten)
-        os.setMAX_RAM_PAGES_PER_PROCESS(10);
+        os.setMAX_RAM_PAGES_PER_PROCESS(20);
         // CLOCK oder FIFO oder RANDOM
-        os.setREPLACEMENT_ALGORITHM(OperatingSystem.ImplementedReplacementAlgorithms.FIFO);
+        os.setREPLACEMENT_ALGORITHM(OperatingSystem.ImplementedReplacementAlgorithms.RANDOM);
         // Anzahl Operationen innerhalb eines Seitenbereichs
-        os.setDEFAULT_LOCALITY_FACTOR(1);
+        os.setDEFAULT_LOCALITY_FACTOR(10);
 
         // Testausgaben erwünscht? Wenn true, dann simulationTime auf max. 200
         // ms setzen!
-        os.setTestMode(true);
+        os.setTestMode(false);
 
         // ------------------------- Parameter setzen Ende
         // -----------------------------------------------
